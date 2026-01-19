8d419a73ba03b98899901164ee5e525c92b7b54f
rdet02 <156576076+urdet@users.noreply.github.com>
2026-01-19 17:50:09 +0100

    Fix des erreur de connection

--- extract.py
+++ extract.py
@@ -1,15 +1,24 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from config import MYSQL_URI
 
 engine = create_engine(MYSQL_URI)
 
-def extract_orders(last_run):
-    query = f"""
-        SELECT order_id, customer_id, quantity, unit_price,
-               status, updated_at
-        FROM orders
-        WHERE updated_at > '{last_run}'
-    """
-    df = pd.read_sql(query, engine)
+def extract_orders(last_run=None):
+    if last_run:
+        query = text("""
+            SELECT order_id, customer_id, quantity, unit_price,
+                   status, updated_at
+            FROM orders
+            WHERE updated_at > :last_run
+        """)
+        df = pd.read_sql(query, engine, params={"last_run": last_run})
+    else:
+        query = text("""
+            SELECT order_id, customer_id, quantity, unit_price,
+                   status, updated_at
+            FROM orders
+        """)
+        df = pd.read_sql(query, engine)
+
     return df
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from extract import extract_orders
 from transform import transform_orders
 from load import load_orders
 from checkpoint import get_last_run, update_last_run
-
+import cryptography
 def run_etl():
     last_run = get_last_run()
 
@@ -17,6 +17,5 @@ def run_etl():
     update_last_run()
 
     print(f"Loaded {len(df)} records")
-
 if __name__ == "__main__":
     run_etl()
